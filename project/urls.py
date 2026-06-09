653fe23c5d407037e0b2edc21e479e08c338b53b
Wagner Vinicius <thomaz_poa@hotmail.com>
2026-06-09 18:31:32 -0300

    Ajustando URL e views inicial

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+from recipes.views import contact, home
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', home),
+    path('contact/', contact)
+
 ]
